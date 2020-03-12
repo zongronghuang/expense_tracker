bd6d68dae0c301e588cbbd65245a98ccd28762d2
Zong-Rong Huang <zongrong.h886@gmail.com>
2020-03-12 23:50:07 +0800

    Add user routes

---
+++ routes/user.js
@@ -0,0 +1,88 @@
+const express = require('express')
+const router = express.Router()
+const User = require('../models/user')
+const passport = require('passport')
+const bcrypt = require('bcryptjs')
+
+// 登入頁面
+router.get('/login', (req, res) => {
+  res.render('login')
+})
+
+// 登入檢查
+router.post('/login', (req, res, next) => {
+  passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/users/login'
+  })(req, res, next)
+})
+
+// 註冊頁面
+router.get('/register', (req, res) => {
+  res.render('register')
+})
+
+// 註冊檢查
+router.post('/register', (req, res) => {
+  const { name, email, password, password2 } = req.body
+
+  let errors = []
+
+  if (!name || !email || !password || !password2) {
+    errors.push({ message: '所有欄位都必填' })
+  }
+
+  if (password !== password2) {
+    errors.push({ message: '密碼錯誤' })
+  }
+
+  if (errors.length > 0) {
+    res.render('register', {
+      errors,
+      name,
+      email,
+      password,
+      password2
+    })
+  } else {
+    User.findOne({ email: email }).then(user => {
+      if (user) {                                       // 檢查 email 是否存在
+        console.log('User already exists')
+        res.render('register', {                // 使用者已經註冊過
+          name,
+          email,
+          password,
+          password2
+        })
+      } else {
+        const newUser = new User({    // 如果 email 不存在就直接新增
+          name,
+          email,
+          password
+        })
+
+        bcrypt.genSalt(10, (err, salt) =>
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err
+            newUser.password = hash
+            newUser
+              .save()
+              .then(user => {
+                res.redirect('/')                         // 新增完成導回首頁
+              })
+              .catch(err => console.log(err))
+          })
+        )
+      }
+    })
+  }
+})
+
+// 登出
+router.get('/logout', (req, res) => {
+  req.logout()
+  req.flash('success_msg', '已登出')
+  res.redirect('/users/login')
+})
+
+module.exports = router
