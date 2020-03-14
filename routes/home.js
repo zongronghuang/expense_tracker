befa3687caf73fa65bf900610104e3a6bf787da2
Zong-Rong Huang <zongrong.h886@gmail.com>
2020-03-15 01:01:21 +0800

    Add personalized display of expense items

--- routes/home.js
+++ routes/home.js
@@ -4,9 +4,12 @@ const Record = require('../models/record.js')
 const { authenticated } = require('../config/auth.js')
 
 router.get('/', authenticated, (req, res) => {
-
-  res.redirect('/records')
-
+  Record.find({ userId: req.user._id })
+    .lean()
+    .exec((err, records) => {
+      if (err) return console.error(err)
+      return res.render('index', { records: records })
+    })
 })
 
 module.exports = router
